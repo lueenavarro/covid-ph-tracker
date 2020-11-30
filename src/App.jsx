fb61497b5c89c3dff540f6d4f378f941c5d1795d
Louie <lnavarro6@dxc.com>
2020-11-30 11:46:55 +0800

    Change icon and title

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Active from "./components/Active";
 import Cases from "./components/Cases";
 import { CovidDataProvider } from "./contexts/CovidData";
 import Deaths from "./components/Deaths";
+import Footer from './components/Footer';
 import React from "react";
 import Recovered from "./components/Recovered";
 import Summary from "./components/Summary";
@@ -13,12 +14,13 @@ function App() {
     <CovidDataProvider>
       <div className="container">
         <Summary />
-      <div className="charts">
-        <Active />
-        <Cases />
-        <Deaths />
-        <Recovered />
-      </div>
+        <div className="charts">
+          <Active />
+          <Cases />
+          <Deaths />
+          <Recovered />
+        </div>
+        <Footer />
       </div>
     </CovidDataProvider>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <p className="footer">
+      Icons made by{" "}
+      <a href="http://www.freepik.com/" title="Freepik">
+        Freepik
+      </a>{" "}
+      from{" "}
+      <a href="https://www.flaticon.com/" title="Flaticon">
+        www.flaticon.com
+      </a>
+    </p>
+  );
+};
+
+export default Footer;
